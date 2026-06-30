9bf53c33da5625b1dcd62cefe64f1b8839772d59
MoraisNie <paulo.henriquecosme2017@gmail.com>
2026-06-30 19:56:59 -0300

    adicionado clientes.java

--- src/Clientes.java
+++ src/Clientes.java
@@ -1,1 +1,33 @@
+//Cliente.java
+package model;
 
+import java.io.Serializable;
+
+public class Cliente implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String nome;
+    private String cpf; // Funciona como ID Único (ou Passaporte/NIF)
+    private String telefone;
+
+    public Cliente(String nome, String cpf, String telefone) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+    }
+
+    // Getters e Setters
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getCpf() { return cpf; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
+
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+
+    @Override
+    public String toString() {
+        return "Cliente: " + nome + " | CPF/ID: " + cpf + " | Tel: " + telefone;
+    }
+}
